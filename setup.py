83a3f5482ad71179db6887aea80e8e98aea9504d
CrushedLive <jordan@bugbird.co>
2022-11-25 20:56:54 +0000

    updating project meta

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup
 
 setup(
-    name='cuoo.thingy',
-    version='0.1',
+    name='cuoo.thingwork',
+    version='0.5.0',
     description='Make anything an IoT thingy',
-    author='Jordan Bugbird',
-    author_email='jordan@cuoo.io',
-    url='https://l.cuoo.io/thingy',
+    author='Bugbird Co.',
+    author_email='hey@cuoo.io',
+    url='https://l.cuoo.io/thingwork',
     packages=['cuoo.thingy.core', 'cuoo.thingy.component', 'cuoo.thingy.web', 'cuoo.thingy.web.controllers',
               'cuoo.thingy.web.middleware'],
     install_requires=[
